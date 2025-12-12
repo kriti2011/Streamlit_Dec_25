4d71cf55665f18050a14811205db526aedb5487e
kriti2011 <kriti.yuva@gmail.com>
2025-12-12 21:39:52 +0530

    Update intro.py

--- intro.py
+++ intro.py
@@ -3,3 +3,18 @@ import streamlit as st
 st.title("Welcome to the Streamlit App!")
 st.subheader("Getting Started with Streamlit")
 st.write("My first Streamlit app is up and running.")
+
+agree=st.checkbox("I agree to the terms and conditions.")
+if agree:
+    st.write("Thank you for agreeing!")
+
+genre=st.radio("Select your favorite genre:", 
+                ["Comedy","Drama","Documentary"])
+if genre=="Comedy":
+    st.write("Hahaha! You selected Comedy.")
+elif genre=="Drama":
+    st.write("Oh no! Drama it is.")
+else:
+    st.write("Documentaries are so informative!")
+
+    
